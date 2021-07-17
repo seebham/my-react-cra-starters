ed45b6a592f651832a1af079de6b377a6eb4f279
seebham <shubhamisdev@gmail.com>
2021-07-17 20:21:59 +0530

    styled-applied

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  text-align: center;
+`;
+
 function App() {
   return (
     <div className="App">
-      <h2>Hey! This is the basic minimal-starter</h2>
-      <ul>
-        <li>Change the title - public/index.html</li>
-        <li>Change Description Meta tag on - public/index.html line: 9</li>
-        <li>Clear this list from src/App.js and start coding!</li>
-      </ul>
+      <AppContainer>
+        <h2>Hey! This is the basic minimal-starter</h2>
+        <ul>
+          <li>Change the title - public/index.html</li>
+          <li>Change Description Meta tag on - public/index.html line: 9</li>
+          <li>Clear this list from src/App.js and start coding!</li>
+        </ul>
+      </AppContainer>
     </div>
   );
 }
