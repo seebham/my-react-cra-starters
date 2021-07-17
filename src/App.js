be562ef492a47dcf6b79dc8a824b00ed71e56e53
seebham <shubhamisdev@gmail.com>
2021-07-17 19:09:37 +0530

    minimized

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 function App() {
-  return <div className="App">Hey! This is the basic minimal-starter</div>;
+  return (
+    <div className="App">
+      <h2>Hey! This is the basic minimal-starter</h2>
+      <ul>
+        <li>Change the title - public/index.html</li>
+        <li>Change Description Meta tag on - public/index.html line: 9</li>
+        <li>Clear this list from src/App.js and start coding!</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
