7d070c598b07a25ecbe451dcaff99751a76cd0c5
seebham <shubhamisdev@gmail.com>
2021-08-01 14:50:36 +0530

    tailwind done

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 <h2 style="text-align: center;">my-react-cra-starters</h2>
 <p>These starters are created from the <a href="https://facebook.github.io/create-react-app/docs/getting-started" target="_blank">Create-React-App Boilerplate</a></p>
+
+### styled-component branch
+
+This branch is minimal-cra + **TailwindCSS**
+
+#### Get Started
+
+1. `git clone -b with-Tailwind https://github.com/seebham/my-react-cra-starters.git [folder_name]`
+2. Happy `react`ing!
--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <div className="App">
-      <h2>Hey! This is the basic minimal-starter</h2>
-      <ul>
+    <div className="container lg mx-auto border-2">
+      <h2 className="text-2xl text-purple-700">
+        Hey! This is the basic minimal-starter
+      </h2>
+      <ul className="list-disc list-inside">
         <li>Change the title - public/index.html</li>
         <li>Change Description Meta tag on - public/index.html line: 9</li>
         <li>Clear this list from src/App.js and start coding!</li>
